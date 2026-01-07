6c2bb904e3f7cfb74ee895918eacd18ebdd09f20
Ahmet Haydar Karabulut <155255217+AhmetHKarabulut@users.noreply.github.com>
2026-01-07 14:48:41 +0300

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,1 +1,8 @@
+import hashlib
+
+def hashed_feature(value: str, num_buckets: int = 10) -> int:
+    """Verilen string'i hashleyip bucket index'i döndürür."""
+    hash_object = hashlib.md5(value.encode())
+    hash_int = int(hash_object.hexdigest(), 16)
+    return hash_int % num_buckets
 
